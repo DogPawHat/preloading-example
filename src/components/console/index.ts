71f17146abcece2450482e6e674b74317955b96f
Ciarán Curley <ciaran.curley.11.11@gmail.com>
2026-04-13 13:45:57 +0100

    imports

--- src/components/console/index.ts
+++
@@ -1,6 +0,0 @@
-export { StatusDot, StatusDotWithLabel } from "./status-dot";
-export { ConsoleCard } from "./console-card";
-export { SectionHeader } from "./section-header";
-export { TypeBadge } from "./type-badge";
-export { CodeButton } from "./code-button";
-export { PokemonTable } from "./pokemon-table";
