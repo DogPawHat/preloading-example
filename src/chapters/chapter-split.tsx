f5b59d10f0c17e16df0b3a36a64e3584867ab3b7
Ciarán Curley <ciaran.curley.11.11@gmail.com>
2026-04-30 21:50:49 +0100

    rename

--- src/chapters/chapter-split.tsx
+++ src/chapters/chapter-split.tsx
@@ -1,4 +1,4 @@
-function BlogTableSplitColumn({ blog, table }: { blog: React.ReactNode; table: React.ReactNode }) {
+function ChapterSplitColumn({ blog, table }: { blog: React.ReactNode; table: React.ReactNode }) {
   return (
     <div className="grid gap-8 xl:grid-cols-[minmax(0,0.9fr)_minmax(34rem,1.1fr)] xl:items-start">
       {blog}
@@ -7,4 +7,4 @@ function BlogTableSplitColumn({ blog, table }: { blog: React.ReactNode; table: R
   );
 }
 
-export { BlogTableSplitColumn };
+export { ChapterSplitColumn };
